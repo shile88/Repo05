c1eb32edeb7f7ad31607b517d3f87b8b01fc0de9
shile88 <milos.ivanis@gmail.com>
2022-04-01 15:57:52 +0200

    N-th character in string

---
+++ ex12/src/index.js
@@ -0,0 +1,10 @@
+function main(){
+
+    var lastName = "Tesla";
+    var thirdLetterOfLastName = lastName[2];
+
+    return thirdLetterOfLastName;
+}
+
+console.log(main());
+module.exports = main;
