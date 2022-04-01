4952f26e494f29cd4d5b375b41543d5c6c2bbbe8
shile88 <milos.ivanis@gmail.com>
2022-04-01 10:38:33 +0200

    Escaping quotes

---
+++ ex02/src/index.js
@@ -0,0 +1,13 @@
+function main(){
+
+    var myFirstString = "I think therefore i \"am\" -- sentient.";
+    var mySecondString = "No, you only \"think\", so you aren't.";
+
+    return {
+        myFirstString,
+        mySecondString
+    };
+}
+
+console.log(main());
+module.exports = main;
