ac38cf287bc8463c6f43fd0983df51fea40ddd6f
shile88 <milos.ivanis@gmail.com>
2022-04-01 10:08:29 +0200

    Adding strings

---
+++ ex01/src/index.js
@@ -0,0 +1,17 @@
+function main(){
+
+    myFirstName = "Milos";
+    myLastName = "Ivanis";
+    myHomeTown = "Podgorica";
+
+    return {
+
+    myFirstName,
+    myLastName,
+    myHomeTown,
+
+    };
+}
+
+console.log(main());
+module.exports = main;
