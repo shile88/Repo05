194fabf1035d2a10dac2cdcd84591d2ae9193e57
shile88 <milos.ivanis@gmail.com>
2022-04-01 11:25:04 +0200

    Appending var to strings

---
+++ ex08/src/index.js
@@ -0,0 +1,12 @@
+function main(){
+
+    var myAdjective = " the best!";
+    var myString = "Mistral is";
+
+    myString += myAdjective;
+
+    return myString;
+}
+
+console.log(main());
+module.exports = main;
