05f8ac46614bec2f9f3a63b4e1cd826ce73003a4
shile88 <milos.ivanis@gmail.com>
2022-04-01 11:21:32 +0200

    Strings with variables

---
+++ ex07/src/index.js
@@ -0,0 +1,10 @@
+function main(){
+
+    var myName = "Milos";
+    var myString = "My name is " + myName + " and i am well!";
+
+    return myString;
+}
+
+console.log(main());
+module.exports = main;
